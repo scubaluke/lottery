c222f7c2ba99abd72a62709ffa100793cc1f2c85
Luke Gartland <lukegartland@gmail.com>
2021-07-23 07:28:19 -1000

    sends money to the winner and resets players array

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -78,4 +78,28 @@ describe('Lottery Contract', () => {
                 assert(error)
             }
         })
+
+        it('sends money to the winner and resets players array', async() => {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei('2', 'ether')
+            })
+
+
+            const initialBalance = await web3.eth.getBalance(accounts[0])
+            await lottery.methods.pickWinner().send({ from: accounts[0] })
+            const finalBalance = await web3.eth.getBalance(accounts[0])
+            const difference = finalBalance - initialBalance;
+
+
+            assert(difference > web3.utils.toWei('1.8', 'ether'))
+
+            const players = await lottery.methods.getPlayers().call({ from: accounts[0] })
+            assert.strictEqual(0, players.length)
+
+            const contractBalance = await web3.eth.getBalance(lottery.options.address)
+            assert.strictEqual('0', contractBalance)
+
+
+        })
 })
